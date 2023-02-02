09ff8d5b20adc857be011cb80c822b4634e8ed4e
LarsD99 <lars.dovle@gokstadakademiet.no>
2023-02-02 23:09:14 +0100

    Endret overskrift

--- visual-studio-github/Program.cs
+++ visual-studio-github/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Heihei!");
+        Console.WriteLine("hva skjer?");
+        Console.WriteLine("gøy!");
 
+    }
+}
